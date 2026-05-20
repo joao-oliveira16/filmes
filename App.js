61cd48c2d23ba05b95ffba2211bc5013aa068de5
joao-oliveira16 <jveo1234@gamil.com>
2026-05-19 21:12:04 -0300

    segundo commit

--- App.js
+++ App.js
@@ -1,72 +1,88 @@
-import { StatusBar} from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image  } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View , TouchableOpacity, TextInput, Image} from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-
-{/* INICIO DA HEADER*/}
- <View style={styles.viewHeader} >
-
- <Feather name="menu" size={24} color="white" />
-
-<Text style={styles.textHeader}> TECFILMES</Text>
-<TouchableOpacity>
- 
-</TouchableOpacity>
-
- </View>
-{/* INICIO DA BARRA DE PESQUISA*/}
-
-<View style={styles.containerSearch}>
-  <TextInput 
-  placeholder='Digite o filme que deseja buscar'
-  ></TextInput>
-
-
-
-</View>
-
-{/* INICIO DO BANNER*/}
+      {/*INICIO DA HEADER*/}
+    <View style={styles.viewHeader}>
+    <Feather name="menu" size={24} color="white" />
+      
+      <Text style={styles.textHeader}> TECFILMES </Text>
+      <TouchableOpacity></TouchableOpacity>
 
     </View>
+
+    {/*INICIO DA BARRA DE PESQUISA*/}
+    <View style={styles.containerSearch}>
+      <TextInput
+      placeholder='Digite o filme que deseja buscar'
+      style={styles.inputSearch}
+      ></TextInput>
+      <TouchableOpacity>
+      <Feather name="search" size={24} color="black"/>
+      </TouchableOpacity>
+    </View>
+
+          {/* Inicio do banner */}
+    
+    <Text style={styles.textBanner}> Em cartaz </Text>
+
+    <Image source={require('./assets/neymar.jpg')} style={styles.imageBanner}/>
+
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#141A29',
+    backgroundColor: '#1E1E1E',
     alignItems: 'center',
-  
   },
-// HEADER
- viewHeader: {
-  flexDirection:'row',
-  width: "90%",
-  alignItems: 'center',
-  marginTop: 10,
-  justifyContent: 'space-between',
-  
-
- },
- textHeader: {
- 
-  fontSize: 25,
-  color: 'white',
-  fontWeight: 'bold'
-
- },
- containerSearch: {
-
-  marginTop: 20,
-  width: "90%",
-  backgroundColor: 'white',
-  borderRadius: 5,
-  padding : 8
-
- },
-
+  // HEADER
+   viewHeader:{
+    flexDirection: 'row',
+    width: "90%",
+    alignItems: 'center',
+    marginTop: 15,
+    justifyContent: 'space-between'
+  },
+  textHeader: {
+    fontSize: 25,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  containerSearch: {
+    marginTop: 20,
+    width:"90%",
+    backgroundColor: 'white',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 5,
+    padding: 8,
+    alignItems: 'center'
+  },
+  inputSearch:{
+    height: 40,
+    padding: 8,
+    width: "100%"
+  },
+  imageBanner:{
+    width: "50%",
+    height: "50%",
+    marginTop: 15,
+    borderRadius: 10,
+  },
+  textBanner: {
+    color: 'white',
+    width: '90%',
+    fontSize: 30,
+    marginTop:20,
+    fontWeight:'bold',
+  }
 });
